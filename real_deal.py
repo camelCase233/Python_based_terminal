25ad79f33a26b84c1761958854b6aba3be695407
camelCase233 <prapti4231@gmail.com>
2025-05-21 14:23:37 +0530

    Update real_deal.py

--- real_deal.py
+++ real_deal.py
@@ -1,11 +1,18 @@
-import os
+# import os
 from pathlib import Path
 import argparse
-import sys
+# import sys
+
+with open('current_dir.txt', 'r') as file:
+    content = file.read()
+    file.seek(0)
+
+
 
 class file_system:
+    current_directory = Path(content)
     def __init__(self):
-        self.curr_dir = Path(os.getcwd())
+        pass
 
 
 class ls(file_system):
@@ -16,18 +23,39 @@ class ls(file_system):
         self.args = self.parser.parse_args()
 
     def run(self):
-        if not self.curr_dir.exists():
-            print(f"The path {self.curr_dir} doesn't exist")
-        elif self.curr_dir.is_file():
-            print(f"{self.curr_dir} is a file")
+        if not file_system.current_directory.exists():
+            return(f"The path {file_system.current_directory} doesn't exist")
+        elif file_system.current_directory.is_file():
+            return(f"{self.curr_dir} is a file")
         elif self.args.list:
-            for file in self.curr_dir.iterdir():
+            for file in file_system.current_directory.iterdir():
                 print(file.name)
                 
         else:
             pass
 
+class cd(file_system):
+    def __init__(self):
+        super().__init__()
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("-cd", "--change_dir", type=str)
+        self.args = self.parser.parse_args()
+    
+    def run(self):
+        if self.args.change_dir:
+            new_path = file_system.current_directory/Path(self.args.change_dir)
+            if new_path.is_file():
+                print("The path is a file, not a directory")
+            
+            elif not new_path.exists():
+                print("The path does not exist")
+            
+            with open('current_dir.txt', 'w') as file:
+                file.write(str(new_path))
+            print("The path has been changed <3")
+
+
 if __name__ == "__main__":
-    cmd = ls()
+    cmd = cd()
     result = cmd.run()
 
